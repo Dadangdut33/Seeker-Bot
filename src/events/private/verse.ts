5dbc5a1b84bf104d9afb4c3d29312943c1487557
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2023-07-15 18:25:35 +0700

    add more schedule and add the tafsir button

--- src/events/private/verse.ts
+++ src/events/private/verse.ts
@@ -1,8 +1,24 @@
-import { Client, TextChannel } from "discord.js";
+import { ActionRowBuilder, ButtonBuilder, Client, TextChannel } from "discord.js";
 import { IBotEvent } from "../../types";
 import { CronJob } from "cron";
 import { embedRandomAyat } from "../../utils/commands/verse";
 import { logger } from "../../logger";
+
+const cronFunc = async (channel: TextChannel) => {
+	const data = await embedRandomAyat();
+	if (!data) return logger.error("[ERROR] [daily-surah] Failed to get random ayat");
+
+	// Surah number and ayat is in the title of the first embed with format like this [surah:number] xxx (xxx) - xxx
+	// get each of it
+	const surahNumber = data[0].data.title?.split(" ")[0].split(":")[0].replace("[", ""),
+		ayatNumber = data[0].data.title?.split(" ")[0].split(":")[1].replace("]", "");
+
+	// add button to get the tafsir
+	const tafsirButton = new ButtonBuilder().setCustomId(`tafsir-${surahNumber}:${ayatNumber}`).setStyle(1).setLabel("Tafsir");
+	const row = new ActionRowBuilder<ButtonBuilder>().addComponents(tafsirButton);
+
+	channel.send({ embeds: data, components: [row] });
+};
 
 const event: IBotEvent = {
 	name: "ready",
@@ -21,22 +37,16 @@ const event: IBotEvent = {
 			if (!channel) return logger.warn("Invalid channel for daily surah");
 
 			try {
-				let scheduledMessage = new CronJob(
-					"30 07 * * *",
-					async () => {
-						// This runs every day at 07:30:00, you can do anything you want
-						const data = await embedRandomAyat();
-						if (!data) return logger.error("[ERROR] [daily-surah] Failed to get random ayat");
+				let scheduledMessages = [
+					// This runs every day at 05:00:00
+					new CronJob("00 05 * * *", async () => await cronFunc(channel), null, true, "Asia/Jakarta"),
+					// This runs every day at 12:00:00
+					new CronJob("00 12 * * *", async () => await cronFunc(channel), null, true, "Asia/Jakarta"),
+					// This runs every day at 16:00:00
+					new CronJob("00 16 * * *", async () => await cronFunc(channel), null, true, "Asia/Jakarta"),
+				];
 
-						channel.send({ embeds: data });
-					},
-					null,
-					true,
-					"Asia/Jakarta"
-				);
-
-				// When you want to start it, use:
-				scheduledMessage.start();
+				scheduledMessages.forEach((element) => element.start());
 			} catch (e) {
 				logger.error(`[ERROR] [daily-surah] ${e}`);
 			}
