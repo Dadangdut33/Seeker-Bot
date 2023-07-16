122efee04933cfb0d090741ad36d3f29b6f5f315
Dadangdut33 <57717531+Dadangdut33@users.noreply.github.com>
2023-07-16 16:21:14 +0700

    fix empty args

--- src/commands/slash/tool/tagspoiler.ts
+++ src/commands/slash/tool/tagspoiler.ts
@@ -5,15 +5,15 @@ const slashCommands: ISlashCommand = {
 	command: new SlashCommandBuilder()
 		.setName("tagspoiler")
 		.setDescription("Gives spoiler warning by reminding people that the text above is full of spoiler")
-		.addStringOption((option) => option.setName("args").setDescription("Description")),
+		.addStringOption((option) => option.setName("description").setDescription("Description")),
 	cooldown: 30,
 	execute: async (interaction) => {
-		const args = interaction.options.getString("args")!;
+		const description = interaction.options.getString("description");
 
 		const embed = new EmbedBuilder()
 			.setColor("Random")
-			.setFooter({ text: args.length > 0 ? `${args} Spoiler Above!!` : interaction.user.username })
-			.setTitle(`SPOILER WARNING!!${args.length > 0 ? `\`${args}\`` : ``} SPOILER ABOVE!!`)
+			.setFooter({ text: description ? `${description} Spoiler Above!!` : interaction.user.username })
+			.setTitle(`SPOILER WARNING!!${description ? `\`${description}\`` : ``} SPOILER ABOVE!!`)
 			.setThumbnail("https://cdn.discordapp.com/attachments/653206818759376916/700992854763372584/Misaka_10777.jpg")
 			.setDescription(
 				"A spoiler is an element of a disseminated summary or description of any piece of fiction that reveals any plot elements which threaten to give away important details. Typically, the details of the conclusion of the plot, including the climax and ending, are especially regarded as spoiler material. It can also be used to refer to any piece of information regarding any part of a given media that a potential consumer would not want to know beforehand. Because enjoyment of fiction depends a great deal upon the suspense of revealing plot details through standard narrative progression, the prior revelation of how things will turn out can ''spoil'' the enjoyment that some consumers of the narrative would otherwise have experienced. -wikipedia.org"
@@ -27,7 +27,7 @@ const slashCommands: ISlashCommand = {
 			.setTimestamp();
 
 		return interaction.reply({
-			content: `Thanks ${interaction.user} \n\n\n**${args.length > 0 ? `\`${args}\` ` : ``}SPOILER** ABOVE!!!\n\n\nScroll up at your own risk`,
+			content: `Thanks ${interaction.user} \n\n\n**${description ? `\`${description}\` ` : ``}SPOILER** ABOVE!!!\n\n\nScroll up at your own risk`,
 			embeds: [embed],
 		});
 	},
